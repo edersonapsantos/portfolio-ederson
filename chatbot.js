c6daf6416d32937ff89c7841d3e6e02a7a0f0e05
Ederson Santos <98858082+edersonapsantos@users.noreply.github.com>
2022-02-02 10:08:12 -0300

    Ajuste ChatBot

--- chatbot.js
+++ chatbot.js
@@ -13,32 +13,8 @@ function responder(){
 
     const pergunta="pergunta";
     const resposta="resposta";
-    //document.getElementById('chatLog').innerHTML = usuario + "<br>";
+
     if(usuario in dialogo){
-        //document.getElementById('chatLog').innerHTML = dialogo[usuario] + "<br>";
-        // cria um novo elemento div
-        // e dá à ele conteúdo
-        //var divNova = document.createElement("p");
-        //var conteudoNovo = document.getElementById('userBox').value;
-        //divNova.appendChild(conteudoNovo);
-        //var divAtual = document.getElementById('chat');
-        //divAtual.appendChild(divNova);
-        //respondendo
-        //var divNovaRes = document.createElement("p");
-        //var conteudoNovoRes = dialogo[usuario] + "<br>";
-        //divNovaRes.appendChild(conteudoNovoRes);
-        //document.body.appendChild(divNova, divAtual);
-        // Buscar elemento pai
-        // cria um novo elemento div
-        // e dá à ele conteúdo
-        /*var chat = document.getElementById('chat');
-        var divNova = document.createElement('p');
-        var conteudoNovo = document.createTextNode(usuario);
-        divNova.appendChild(conteudoNovo); //adiciona o nó de texto à nova div criada
-    
-        // adiciona o novo elemento criado e seu conteúdo ao DOM
-        var divAtual = document.getElementById('chatLog');
-        chat.insertBefore(divNova, divAtual);*/
         adcElemento(usuario, pergunta);
         adcElemento(dialogo[usuario], resposta);
 
